ace22eedae46b6d6ad775a4075b157b145c9be86
Vktoriia <natasukhanova18@gmail.com>
2026-03-17 00:09:33 +0100

    finish
    jfhfhgfv

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,24 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
-
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
-
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const menuBtnOpen = document.querySelector('.menu-btn-open');
+    const menuBtnClose = document.querySelector('.menu-btn-close');
+    const menuLinks = document.querySelectorAll('.mobile-menu__link');
+  
+    const openMenu = () => {
+      mobileMenu.classList.add('is-open');
+    };
+  
+    const closeMenu = () => {
+      mobileMenu.classList.remove('is-open');
+    };
+  
+    menuBtnOpen.addEventListener('click', openMenu);
+    menuBtnClose.addEventListener('click', closeMenu);
+  
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        closeMenu();
+      });
+    });
+  });
+  
